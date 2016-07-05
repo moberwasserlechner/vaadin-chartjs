95de76634186d073151cf5acda75d8521bc91517
Michael Oberwasserlechner <michaelowl@gmail.com>
2016-07-06 00:47:07 +0200

    Add js logging setter/getter

--- addon/src/main/java/com/byteowls/vaadin/chartjs/ChartJs.java
+++ addon/src/main/java/com/byteowls/vaadin/chartjs/ChartJs.java
@@ -7,6 +7,8 @@ import com.vaadin.ui.AbstractJavaScriptComponent;
 @JavaScript({"vaadin://chartjs/Chart.min.js", "vaadin://chartjs/chartjs-connector.js"})
 public class ChartJs extends AbstractJavaScriptComponent {
 
+    private static final long serialVersionUID = 2999562112373836140L;
+
     public ChartJs(ChartConfig chartConfig) {
         configure(chartConfig);
     }
@@ -20,6 +22,21 @@ public class ChartJs extends AbstractJavaScriptComponent {
         if (chartConfig != null) {
             getState().configurationJson = chartConfig.buildJson();
         }
+    }
+    
+    /**
+     * @return True if the connector's logs defined messages to "console.log" else logging is disabled.
+     */
+    public boolean isJsLoggingEnabled() {
+      return getState().loggingEnabled;
+    }
+
+    /**
+     * Enable or disables the connector's logging to "console.log"
+     * @param jsLoggingEnabled If true the connector script will log defined messages to "console.log". Defaults to false. 
+     */
+    public void setJsLoggingEnabled(boolean jsLoggingEnabled) {
+      getState().loggingEnabled = jsLoggingEnabled;
     }
 
     @Override
